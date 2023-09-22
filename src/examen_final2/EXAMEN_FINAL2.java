1e61e2836495bbd8a003b3f81c20dd6571fe65ac
MARTI <MARTI@MARTINX>
2023-09-22 16:33:13 -0600

    METER

---
+++ src/examen_final2/EXAMEN_FINAL2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen_final2;
+
+/**
+ *
+ * @author MARTI
+ */
+public class EXAMEN_FINAL2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
